ee1545feaba4e0f60267dbfe2cf20dc504cb2aa9
zjj9788 <zjj9788@163.com>
2019-05-21 17:22:56 +0800

    laozhang

--- EWS/src/main/java/com.isoft.action/UserAction.java
+++ EWS/src/main/java/com.isoft.action/UserAction.java
@@ -1,5 +1,8 @@
 package com.isoft.action;
 
+import com.isoft.service.IUserService;
+import com.isoft.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,24 +10,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/user")
 public class UserAction {
+    @Autowired
+    IUserService userService;
     @RequestMapping("/login.do")
     @ResponseBody
     public String login(String uname, String upwd) {
         /*硬编码*/
-        if (uname.equals("admin") && upwd.equals("123456"))
-            return "success";
-        else
-            return "fault";
+        String str = userService.login(uname, upwd);
+        return str;
 
     }
 
     @RequestMapping("/register.do")
     @ResponseBody
-    public String register(String uname, String upwd,String email) {
+    public String register(String uname, String upwd, String email) {
         /*硬编码*/
-        System.out.println(email);
-        boolean temp=false;
-        if (temp)
+        boolean temp = userService.register(uname,upwd,email);
+          if (temp)
             return "success";
         else
             return "fault";
---
+++ EWS/src/main/java/com.isoft.dao/impl/UserDAOImpl.java
@@ -0,0 +1,51 @@
+package com.isoft.dao.impl;
+
+import com.isoft.dao.IUserDAO;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository("userDAO")
+public class UserDAOImpl implements IUserDAO {
+    @Autowired
+    SqlSessionFactory sessionFactoryBean;
+
+    @Override
+    public String login(String uname, String upwd) throws Exception {
+
+        SqlSession sqlSession = sessionFactoryBean.openSession(true);//true  开启事务
+        String statment = "com.isoft.mapping.userMapper.login";
+        Map map = new HashMap();
+        map.put("uname", uname);
+        map.put("upwd", upwd);
+        System.out.println(map);
+        List list = sqlSession.selectList(statment, map);
+        System.out.println(list);
+        if (list != null)
+            return "success";
+        else
+            return "fault";
+    }
+
+    @Override
+    public boolean register(String uname, String upwd, String email) {
+        SqlSession sqlSession = sessionFactoryBean.openSession(true);
+        String statement = "com.isoft.mapping.userMapper.register";
+        Map map = new HashMap();
+        map.put("uname", uname);
+        map.put("upwd", upwd);
+        map.put("email", email);
+        int i = sqlSession.insert(statement, map);
+        if (i > 0)
+            return true;
+        else
+            return false;
+    }
+}
